471eccc1846f58fb76cd47fc9b7716525516d3f3
vishnupriya <34645232+vishnupriyaravi22@users.noreply.github.com>
2018-07-03 09:39:15 -0700

    Create 7 BYTES.C

---
+++ PRO SET 1/7 BYTES.C
@@ -0,0 +1,19 @@
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+		int n=s.nextInt();
+		if(n%2==0)
+		{
+			System.out.print(0);	
+		}
+		else
+		{
+			System.out.print(1);
+		}
+		s.close();
+	}
+}
