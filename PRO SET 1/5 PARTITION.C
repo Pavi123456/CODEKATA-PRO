5aa5d03c8c38edb04ea521fb12d9bce0aa8b02ef
vishnupriya <34645232+vishnupriyaravi22@users.noreply.github.com>
2018-07-03 08:49:32 -0700

    Create 5 PARTITION.C

---
+++ PRO SET 1/5 PARTITION.C
@@ -0,0 +1,29 @@
+
+import java.util.*;
+
+public class Main {
+	public static void main (String[] args)
+	{
+	Scanner sc=new Scanner(System.in);
+	int n=sc.nextInt();
+	int a=sc.nextInt();
+	int b=sc.nextInt();
+	int sum=0;
+	if(n%2==0)
+	{
+	    sum=a+b;
+	    if(n%sum==0)
+	    {
+	        System.out.print("YES");
+	    }
+	    else
+	    {
+	        System.out.print("NO");  
+	    }
+	}
+	else
+	{
+	    System.out.print("NO");    
+	}
+	}
+}
