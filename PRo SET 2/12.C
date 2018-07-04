24cf7c19232f17522df75f33262002956cd633b8
vishnupriya <34645232+vishnupriyaravi22@users.noreply.github.com>
2018-07-03 22:43:33 -0700

    Update 12.C

--- PRo SET 2/12.C
+++ PRo SET 2/12.C
@@ -1,27 +1,28 @@
-#include <stdio.h>
+#include<stdio.h>
 int main()
 {
-	int n,q,u,v,arr[10],i,j,k;
-	scanf("%d %d",&n,&q);
-	for(i=1;i<=n;i++)
-	{
-	    scanf("%d",&arr[i]);
-	}
-	for(k=1;k<=q;k++)
-	{
-	    scanf("%d %d",&u[k],&v[k]);
-	}
-		for(k=1;k<=q;k++)
-	{
-	     int sum=0;
-	    for(i=u[k];i<=v[k];i++)
-	    {
-	       
-	    sum=sum+arr[i];
-	    
-	    }
-	    printf("%d\n",sum);
-	}
+int num[30];
+int t,j,a,n;
 
-	    return 0;
+scanf("%d", &n);
+
+for(t=0;t<n;++t)
+scanf("%d",&num[t]);
+for(t=0;t<n;++t)
+{
+for(j=t+1;j<n;++j)
+{
+if(num[t]<num[j])
+{
+a=num[t];
+num[t]=num[j];
+num[j]=a;
+}
+}
+}
+
+for(t=0;t<n;++t)
+{
+printf("%d\n",num[t]);
+}
 }
